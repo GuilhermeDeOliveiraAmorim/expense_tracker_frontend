9475ca6c29c7aad1a674d472c500d11c0e4a4dbe
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-09-02 18:14:28 -0300

    fix: create functions to convert rgb to hex

--- components/category/addcategory.tsx
+++ components/category/addcategory.tsx
@@ -1,17 +1,39 @@
 import { Button, ColorPicker, Form, Input, message } from "antd";
 import { CreateCategoryInputDTO } from "../../internal/usecases/create_category";
 import { CategoryFactory } from "../../internal/factory/category.factory";
+import { rgbToHex } from "../../internal/util/functions";
+
+type colorObject = {
+  cleared: boolean;
+  metaColor: {
+    isValid: boolean;
+    r: number;
+    g: number;
+    b: number;
+    a: number;
+    _h: number;
+    _s: number;
+    _v: number;
+  };
+};
 
 const AddCategory = () => {
-  const onFinish = async (values: CreateCategoryInputDTO) => {
+  const onFinish = async (values: { name: string; metaColor: colorObject }) => {
     try {
       const categoryFactory = new CategoryFactory();
 
       const createCategoryUseCase = categoryFactory.createCategoryUseCase();
 
-      console.log(values.color);
+      const input: CreateCategoryInputDTO = {
+        name: values.name,
+        color: rgbToHex(
+          values.metaColor.metaColor.r,
+          values.metaColor.metaColor.g,
+          values.metaColor.metaColor.b
+        ),
+      };
 
-      const response = await createCategoryUseCase.execute(values);
+      const response = await createCategoryUseCase.execute(input);
 
       message.success(response.message);
     } catch (error) {
@@ -32,8 +54,8 @@ const AddCategory = () => {
         <Input placeholder="Category" />
       </Form.Item>
 
-      <Form.Item
-        name="color"
+      <Form.Item<colorObject>
+        name={"metaColor"}
         rules={[{ required: true, message: "Please input category color!" }]}
       >
         <ColorPicker size="small" showText />
