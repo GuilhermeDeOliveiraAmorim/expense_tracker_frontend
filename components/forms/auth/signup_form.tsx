4b13f9dcbb03596cc92b4cbe3d82612e49a292b1
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-09-06 14:38:01 -0300

    feat: add signup gateway

--- components/forms/auth/signup_form.tsx
+++ components/forms/auth/signup_form.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import {
@@ -9,24 +11,100 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
+import { useToast } from "@/hooks/use-toast";
+import { useState } from "react";
+import { UserFactory } from "@/internal/factory/user.factory";
+import { CreateUserInputDTO } from "@/internal/usecases/create_user";
+import { useRouter } from "next/navigation";
 
 export default function SignupForm() {
+  const { toast } = useToast();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (!name || !email || !password) {
+      toast({
+        variant: "destructive",
+        title: "Name, email and password required",
+        description: "Name, email and password required for create an account",
+      });
+      return;
+    }
+
+    try {
+      const input: CreateUserInputDTO = {
+        name,
+        email,
+        password,
+      };
+
+      const userFactory = new UserFactory();
+      const createUserUseCase = userFactory.createUserUseCase();
+
+      const response = await createUserUseCase.execute(input);
+
+      toast({
+        variant: "default",
+        title: response.user_id,
+        description: response.message,
+        style: {
+          backgroundColor: "#4ade80",
+        },
+        duration: 2000,
+      });
+
+      setTimeout(() => {
+        router.push("/login");
+      }, 2000);
+    } catch (error) {
+      if (error instanceof Error) {
+        toast({
+          variant: "destructive",
+          title: "Error",
+          description: error.message,
+        });
+      } else {
+        toast({
+          variant: "default",
+          title: "Error",
+          description: "An unexpected error occurred",
+        });
+      }
+    }
+  };
+
   return (
     <Card className="w-[350px]">
       <CardHeader>
         <CardTitle>Create an account</CardTitle>
         <CardDescription>Understand your finances.</CardDescription>
       </CardHeader>
-      <CardContent>
-        <form>
+      <form onSubmit={handleSubmit}>
+        <CardContent>
           <div className="grid w-full items-center gap-4">
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Your name here" />
+              <Input
+                id="name"
+                placeholder="Your name here"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="framework">Email</Label>
-              <Input id="email" placeholder="Your email here" />
+              <Input
+                id="email"
+                placeholder="Your email here"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
             <div className="flex flex-col space-y-1.5">
               <Label htmlFor="framework">Password</Label>
@@ -34,14 +112,16 @@ export default function SignupForm() {
                 id="password"
                 type="password"
                 placeholder="Create a strong password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
               />
             </div>
           </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button>Register</Button>
-      </CardFooter>
+        </CardContent>
+        <CardFooter className="flex justify-between">
+          <Button>Register</Button>
+        </CardFooter>
+      </form>
     </Card>
   );
 }
