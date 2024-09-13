74aa9bb00cd00eced0f5c2832b41613e4b16e44c
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-09-12 21:33:44 -0300

    feat: add other multiple selector

--- components/forms/expense/add_expense_form.tsx
+++ components/forms/expense/add_expense_form.tsx
@@ -10,7 +10,7 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
-import { useEffect, useState } from "react";
+import { ComponentType, useEffect, useState } from "react";
 import { useToast } from "@/hooks/use-toast";
 import { format } from "date-fns";
 import { Icons } from "@/components/ui/icons";
@@ -37,26 +37,23 @@ import { Calendar } from "@/components/ui/calendar";
 import { GetCategoriesOutputDTO } from "@/internal/usecases/get_categories";
 import { ExpenseFactory } from "@/internal/factory/expense.factory";
 import { Category } from "@/internal/domain/category";
-import MultipleSelector, { Option } from "@/components/ui/multipleselector";
+import { TagFactory } from "@/internal/factory/tag.factory";
+import { MultiSelect } from "@/components/ui/multipleselector";
 
-const OPTIONS: Option[] = [
-  { label: "nextjs", value: "Nextjs" },
-  { label: "Vite", value: "vite", disable: true },
-  { label: "Nuxt", value: "nuxt", disable: true },
-  { label: "Vue", value: "vue, disable: true", disable: true },
-  { label: "Remix", value: "remix" },
-  { label: "Svelte", value: "svelte", disable: true },
-  { label: "Angular", value: "angular", disable: true },
-  { label: "Ember", value: "ember", disable: true },
-  { label: "React", value: "react" },
-  { label: "Gatsby", value: "gatsby", disable: true },
-  { label: "Astro", value: "astro", disable: true },
-];
+type Option = {
+  label: string;
+  value: string;
+  icon?: ComponentType<{ className?: string | undefined }> | undefined;
+};
+
+type Options = Option[];
 
 export default function AddExpenseForm() {
   const { toast } = useToast();
 
   const [categories, setCategories] = useState<GetCategoriesOutputDTO>();
+  const [tags, setTags] = useState<Options>();
+  const [selectedTags, setSelectedTags] = useState<string[]>([]);
   const [amount, setAmount] = useState("");
   const [date, setDate] = useState<Date>();
   const [notes, setNotes] = useState("");
@@ -114,10 +111,73 @@ export default function AddExpenseForm() {
     fetchCategories();
   }, [toast]);
 
+  useEffect(() => {
+    const user_id = sessionStorage.getItem("user_id");
+
+    if (user_id === null || user_id === undefined) {
+      toast({
+        variant: "destructive",
+        title: "Error",
+        description: "User not authenticated",
+        action: <Icons.alert className="mr-2 h-4 w-4" />,
+        duration: 1500,
+      });
+      return;
+    }
+
+    const fetchTags = async () => {
+      try {
+        const tagsFactory = new TagFactory();
+        const tags = await tagsFactory.getTagsUseCase().execute({
+          user_id: user_id,
+        });
+
+        if (tags.tags.length > 0) {
+          const tagsOptions: Options = [];
+
+          tags.tags.forEach((tag) => {
+            const tagOption = {
+              label: tag.name,
+              value: tag.id,
+            };
+
+            tagsOptions.push(tagOption);
+          });
+
+          if (tagsOptions != undefined) {
+            setTags(tagsOptions);
+          }
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          toast({
+            variant: "destructive",
+            title: "Error",
+            description: error.message,
+            action: <Icons.alert className="mr-2 h-4 w-4" />,
+            duration: 1500,
+          });
+          return;
+        } else {
+          toast({
+            variant: "default",
+            title: "Error",
+            description: "An unexpected error occurred",
+            action: <Icons.alert className="mr-2 h-4 w-4" />,
+            duration: 1500,
+          });
+          return;
+        }
+      }
+    };
+
+    fetchTags();
+  }, [toast]);
+
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    if (!amount || !notes || !date || !categoryId) {
+    if (!amount || !date || !categoryId) {
       toast({
         variant: "destructive",
         title: "All fields are required",
@@ -148,11 +208,7 @@ export default function AddExpenseForm() {
         expense_date: format(date, "ddMMyyyy"),
         category_id: categoryId,
         notes: notes,
-        tags: [
-          "01J7KWKVYN14MQ0BZA3JB74ZQV",
-          "01J7KWP6GG5Z7EPD0WEV50KK0V",
-          "01J7KWSEM322CDBJVW5F2381VB",
-        ],
+        tags: selectedTags,
       };
 
       const expenseFactory = new ExpenseFactory();
@@ -176,6 +232,8 @@ export default function AddExpenseForm() {
       setNotes("");
       setCategoryId("");
       setCategories(categories);
+      setTags(tags);
+      setSelectedTags([]);
     } catch (error) {
       if (error instanceof Error) {
         toast({
@@ -269,14 +327,15 @@ export default function AddExpenseForm() {
                 </SelectGroup>
               </SelectContent>
             </Select>
-            <MultipleSelector
-              defaultOptions={OPTIONS}
-              placeholder="Select frameworks you like..."
-              emptyIndicator={
-                <p className="text-center text-lg leading-10 text-gray-600 dark:text-gray-400">
-                  no results found.
-                </p>
+            <MultiSelect
+              options={tags || []}
+              onValueChange={(selectedValues) =>
+                setSelectedTags(selectedValues)
               }
+              defaultValue={selectedTags}
+              placeholder="Select tags"
+              variant="inverted"
+              maxCount={3}
             />
             <Textarea
               name="notes"
