2e421fd54d28c542a229dd4999469dd29f1fc409
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-09-16 19:37:38 -0300

    feat: add w-fit to FormDialog

--- components/ui/formdialog.tsx
+++ components/ui/formdialog.tsx
@@ -18,9 +18,11 @@ export default function FormDialog(props: FormDialogProps) {
       <AlertDialogTrigger asChild>
         <Button variant="outline">+</Button>
       </AlertDialogTrigger>
-      <AlertDialogContent className="fixed bg-white justify-center border-none left-[50%] top-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] gap-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
+      <AlertDialogContent className="w-fit fixed shadow-none bg-white justify-center border-none left-[50%] top-[50%] z-50 grid translate-x-[-50%] translate-y-[-50%] gap-6 duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg">
         {form}
-        <AlertDialogCancel>Cancel</AlertDialogCancel>
+        <AlertDialogCancel className="bg-[#171717] text-white border-none hover:bg-primary/90 hover:text-white">
+          Cancel
+        </AlertDialogCancel>
       </AlertDialogContent>
     </AlertDialog>
   );
