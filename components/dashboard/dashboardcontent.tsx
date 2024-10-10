85702ea20fd531d950005ee902b424d86cde059a
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-10-10 20:57:23 -0300

    fix: adjusment to footer and dashboard content

--- components/dashboard/dashboardcontent.tsx
+++ components/dashboard/dashboardcontent.tsx
@@ -1,14 +1,13 @@
 "use client";
 
 import GetTotalExpensesForPeriodForm from "../forms/presenters/get_total_expenses_for_period_form";
+import GetExpensesByCategoryPeriodForm from "../forms/presenters/get_expenses_by_category_period_form";
+import GetExpensesByMonthYearForm from "../forms/presenters/get_expenses_by_month_year_form";
 import { Icons } from "@/components/ui/icons";
 import { toast } from "@/hooks/use-toast";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { PageContentProps } from "@/props_types/props.types";
-import GetExpensesByCategoryPeriodForm from "../forms/presenters/get_expenses_by_category_period_form";
-import GetMonthlyExpensesByCategoryYearForm from "../forms/presenters/get_monthly_expenses_year_form";
-import GetExpensesByMonthYearForm from "../forms/presenters/get_expenses_by_month_year_form";
 
 export default function DashboardContent({ header, footer }: PageContentProps) {
   const router = useRouter();
@@ -54,12 +53,13 @@ export default function DashboardContent({ header, footer }: PageContentProps) {
 
       <main className="flex flex-col bg-gray-100 pl-48 pr-48 pt-6 pb-6 gap-6 w-full h-full">
         <div className="flex flex-row gap-4 w-full">
-          <GetExpensesByMonthYearForm />
-        </div>
-        <div className="flex flex-row gap-4 w-full h-full">
-          <GetTotalExpensesForPeriodForm />
-          <GetExpensesByCategoryPeriodForm />
-          <GetMonthlyExpensesByCategoryYearForm />
+          <div className="flex flex-col gap-4 w-3/12">
+            <GetTotalExpensesForPeriodForm />
+            <GetExpensesByCategoryPeriodForm />
+          </div>
+          <div className="w-9/12 h-full">
+            <GetExpensesByMonthYearForm />
+          </div>
         </div>
       </main>
 
--- components/layout/footer/footer.tsx
+++ components/layout/footer/footer.tsx
@@ -2,7 +2,7 @@
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-600 text-white p-4 text-center">
+    <footer className="bg-[#171717] text-white p-4 text-center">
       <p>
         &copy;
         {new Date().getFullYear()} Meu Site. Todos os direitos reservados.
