840aa0aa7456492fff10b02006161e0c0e8a95df
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-11-01 12:21:58 -0300

    :sparkles: feat: add SideMenuButton component

--- app/newcomponents/page.tsx
+++ app/newcomponents/page.tsx
@@ -2,6 +2,8 @@
 
 import GraphButton from "@/components/ui/buttons/graph_button";
 import NextPrevButton from "@/components/ui/buttons/next_prev_button";
+import SideMenuButton from "@/components/ui/buttons/side_menu_button";
+import { Icons } from "@/components/ui/icons";
 
 export default function NewComponents() {
   return (
@@ -9,6 +11,23 @@ export default function NewComponents() {
       <GraphButton text="View Graph" />
       <NextPrevButton isNext={false} onClick={() => console.log("Prev")} />
       <NextPrevButton isNext={true} onClick={() => console.log("Next")} />
+      <div className="flex flex-col gap-2">
+        <SideMenuButton
+          icon={<Icons.dollarSign />}
+          onClick={() => console.log("Adicionar Despesas")}
+          label="Adicionar Despesas"
+        />
+        <SideMenuButton
+          icon={<Icons.settings />}
+          onClick={() => console.log("Configurações")}
+          label="Configurações"
+        />
+        <SideMenuButton
+          icon={<Icons.power />}
+          onClick={() => console.log("Fechar")}
+          label="Fechar"
+        />
+      </div>
     </main>
   );
 }
---
+++ components/ui/buttons/side_menu_button.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import { Button } from "../button";
+
+type SideMenuButtonProps = {
+  onClick: () => void;
+  icon: React.ReactNode;
+  label: string;
+};
+
+export default function SideMenuButton({
+  icon,
+  label,
+  onClick,
+}: SideMenuButtonProps) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
+  return (
+    <div
+      className="flex flex-row items-center w-full p-2 gap-2 bg-[#134074] rounded-[12px]"
+      onClick={onClick}
+    >
+      <div className="text-[#8DA9C4]">{icon}</div>
+      <Button
+        className={`${
+          isClicked
+            ? "w-full bg-[#8DA9C4] text-[#0B2545] font-semibold hover:bg-[#97b2ca] hover:text-[#0B2545] rounded-[6px]"
+            : "w-full bg-[#0B2545] text-[#EEF4ED] font-semibold hover:bg-[#13315C] hover:text-[#EEF4ED] rounded-[6px]"
+        }`}
+        onClick={handleClick}
+      >
+        {label}
+      </Button>
+    </div>
+  );
+}
