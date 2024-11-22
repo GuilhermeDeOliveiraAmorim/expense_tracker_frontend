47b999e96a5fe87497a18e091760b8b969d8b25f
GuilhermeDeOliveiraAmorim <guilherme.o.a.ufal@gmail.com>
2024-11-21 21:35:34 -0300

    🐛 fix: adjust DayToDayExpensesPeriod to handle exceptions

--- components/ui/day_to_day_expenses_period/day_to_day_expenses_period.tsx
+++ components/ui/day_to_day_expenses_period/day_to_day_expenses_period.tsx
@@ -1,7 +1,7 @@
 import GraphButton from "../buttons/graph_button";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DayToDayExpensesPeriodChart } from "./day_to_day_expenses_period_chart";
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { getDayToDayExpensesPeriod } from "@/components/query_functions/qf.presenters";
 import { rangerDate } from "@/components/util/date.handler";
 import {
@@ -11,6 +11,7 @@ import {
 } from "@/internal/presenters/get_day_to_day_expenses_period";
 import { format } from "date-fns";
 import { ChartDataItem } from "../amount_month_by_month_in_a_year/amount_month_by_month_in_a_year_chart";
+import { IconSpinner } from "../iconspinner";
 
 export function DayToDayExpensesPeriod() {
   const [activeButton, setActiveButton] = useState<"bt1" | "bt2" | "bt3">(
@@ -34,6 +35,19 @@ export function DayToDayExpensesPeriod() {
     }));
   };
 
+  const {
+    data: expensesData,
+    error: expensesError,
+    isLoading: expensesLoading,
+  } = useQuery({
+    queryKey: ["getDayToDayExpensesPeriod", "getDayToDayExpensesPeriod"],
+    queryFn: () =>
+      getDayToDayExpensesPeriod({
+        startDate: calculateStartDate(1),
+        endDate: rangerDate({ today: true }),
+      }),
+  });
+
   const mutation = useMutation<
     GetDayToDayExpensesPeriodOutputDTO,
     Error,
@@ -44,6 +58,7 @@ export function DayToDayExpensesPeriod() {
     onSuccess: (output: GetDayToDayExpensesPeriodOutputDTO) => {
       if (output && output.expenses) {
         const formattedData = convertToChartData(output.expenses);
+        setDayToDayExpenses(output.expenses);
         setChartData(formattedData);
       } else {
         setDayToDayExpenses([]);
@@ -51,6 +66,16 @@ export function DayToDayExpensesPeriod() {
     },
     onError: () => setDayToDayExpenses([]),
   });
+
+  useEffect(() => {
+    if (!expensesLoading) {
+      if (expensesData?.expenses) {
+        const formattedData = convertToChartData(expensesData.expenses);
+        setDayToDayExpenses(expensesData.expenses);
+        setChartData(formattedData);
+      }
+    }
+  }, [expensesData?.expenses, expensesLoading]);
 
   const handleButtonClick = (button: "bt1" | "bt2" | "bt3", weeks: number) => {
     setActiveButton(button);
@@ -65,27 +90,40 @@ export function DayToDayExpensesPeriod() {
     });
   };
 
-  console.log(dayToDayExpenses);
-
   return (
     <div className="flex flex-col gap-2 bg-[#EEF4ED] rounded-[12px] shadow-md pt-4 justify-between">
-      <DayToDayExpensesPeriodChart data={chartData} />
+      {expensesError ? (
+        <div className="flex justify-center items-center h-full w-full">
+          Error loading data
+        </div>
+      ) : expensesLoading ? (
+        <div className="flex justify-center items-center h-full w-full">
+          <IconSpinner />
+        </div>
+      ) : dayToDayExpenses.length === 0 ? (
+        <div className="flex justify-center items-center h-full w-full">
+          No expenses found
+        </div>
+      ) : (
+        <DayToDayExpensesPeriodChart data={chartData} />
+      )}
+
       <div className="flex flex-row gap-2 p-2 w-full">
         <GraphButton
           text="1W"
-          height={30}
+          height={50}
           onClick={() => handleButtonClick("bt1", 1)}
           isClicked={activeButton === "bt1"}
         />
         <GraphButton
           text="2W"
-          height={30}
+          height={50}
           onClick={() => handleButtonClick("bt2", 2)}
           isClicked={activeButton === "bt2"}
         />
         <GraphButton
           text="3W"
-          height={30}
+          height={50}
           onClick={() => handleButtonClick("bt3", 3)}
           isClicked={activeButton === "bt3"}
         />
